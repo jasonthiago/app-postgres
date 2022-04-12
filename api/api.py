76d187a96bbe28d02d8bd33880a8f7c402db260b
Jasom Alencar <jasonthiago@gmail.com>
2022-04-11 23:28:41 -0300

    Adds API on compose

--- api/api.py
+++ api/api.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
+import os
 import json
 import traceback
 import base64
 import logging
+import psycopg2
 from datetime import date, datetime
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='postgres',
+                            user='postgres',
+                            password='fiap123')
+    return conn
+
 @app.route("/")
-def hello():
-    return "Hello World!\n"
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM links;')
+    links = cur.fetchall()
+    cur.close()
+    conn.close()
+    return "Rodou!"
 
 @app.route("/Teste")
 def Teste():
